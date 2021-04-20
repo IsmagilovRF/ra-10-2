38193a97fef75fc81674c9a0f4d966dc9bd46a85
IsmagilovRF <ledohod@icloud.com>
2021-04-20 11:10:30 +0300

    version 1.0.1

---
+++ src/reducers/serviceFilter.js
@@ -0,0 +1,17 @@
+import { FILTER_SERVICE_FIELD } from "../actions/actionTypes.js";
+
+const initialState = {
+  value: "",
+};
+
+export default function serviceFilterReducer(state = initialState, action) {
+
+  switch (action.type) {
+    case FILTER_SERVICE_FIELD:
+      const { value } = action.payload;
+      return { value };
+      // return {...initialState}
+    default:
+      return state;
+  }
+}
